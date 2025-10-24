418958de924e14476841d992ef5fb5b052f2d9ae
Allan-Binga <allanbinga73@gmail.com>
2025-10-24 07:10:35 -0400

    First deployment

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("Incoming request origin:", origin)
+    // console.log("Incoming request origin:", origin)
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
