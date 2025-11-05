87035d79a8b30a1eebd682c4a3fdaf5983f8c42c
Allan-Binga <allanbinga73@gmail.com>
2025-11-05 08:42:59 -0500

    Loggin the origin

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    // console.log("Incoming request origin:", origin)
+    console.log("Incoming request origin:", origin)
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
